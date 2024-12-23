cc40bce6299b485f4c629d20bb0b5320fafe062d
dementeddr <2413901+dementeddr@users.noreply.github.com>
2024-12-23 11:01:37 -0500

    day 9 still doesn't quite work. Going to switch tactics

--- 9day/9-1.py
+++ 9day/9-1.py
@@ -16,87 +16,72 @@ def main(data):
     max_id = math.ceil(len(line)/2)-1
     start_id = 0
     end_id = max_id
+    end_pos = len(line) - 1
     cur_pos = 0
     checksum = 0
     block_queue = deque()
     space_queue = deque()
 
-    end = {}
+    #end = {}
 
     print(f"{len(line)} -> {end_id}")
+
+    for _ in range(int(line[-1])):
+        #print(f"enqueue: end block {max_id}")
+        block_queue.append(max_id)
+
+    #print()
 
     for i in range(0, len(line)-1, 2):
 
         s_file = int(line[i])
         s_space = int(line[i+1])
 
-        e_file = int(line[-i-1])
-        e_space = -1 if i == 0 else int(line[-i])
-        
-        print(f"{i} [{start_id}:{end_id}]: {s_file} {s_space}, {e_file} {e_space} | {cur_pos} {checksum}")
+        #print(f"[{i} -> {start_id}]: {s_file} {s_space} | {cur_pos} {checksum}")
 
-        for _ in range(s_file):
-            checksum += cur_pos * start_id
-            end[cur_pos] = hex(start_id)[-1]
-            print(f"  s_file: {cur_pos} * {start_id} = {checksum}")
-            cur_pos += 1
-
-        if i <= max_id:
-            for _ in range(e_file):
-                print(f"  e_file: block in {end_id}")
-                block_queue.append(end_id)
+        if i < end_pos:
+            for _ in range(s_file):
+                checksum += cur_pos * start_id
+                #end[cur_pos] = hex(start_id)[-1]
+                #print(f"  s_file: {cur_pos} * {start_id} = {checksum}")
+                cur_pos += 1
 
         for _ in range(s_space):
-            print(f"  s_spac: space in {cur_pos}")
+            #print(f"  s_spac: space in {cur_pos}")
             space_queue.append(cur_pos)
             cur_pos += 1
+
+        while len(space_queue) > len(block_queue) and i < end_pos:
+            end_pos -= 2
+            end_id -= 1
+            e_file = int(line[end_pos])
+            #print(f"  e_file: [{end_pos} -> {end_id}]")
+
+            for _ in range(e_file):
+                #print(f"    enqueue: block in {end_id}")
+                block_queue.append(end_id)
         
-        for _ in range(min(s_space, len(block_queue), len(space_queue))):
+        for _ in range(min(len(block_queue), len(space_queue))):
             block = block_queue.popleft()
             space = space_queue.popleft()
             checksum += block * space
-            end[space] = hex(block)[-1]
-            print(f"  queues: {block} * {space} = {checksum}")
+            #end[space] = hex(block)[-1]
+            #print(f"  queues: {block} * {space} = {checksum}")
+        
 
         start_id += 1
-        end_id -= 1
-        print()
+        #print()
 
-        if i > max_id and len(block_queue) == 0:
+        if i >= end_pos and len(block_queue) == 0:
             break
 
-    print(f"{max_id} [5]: {line[max_id]} | {cur_pos} {checksum}")
+    #keys = sorted(end)
+    #out = ''
 
-    '''
-    for _ in range(int(line[max_id])):
-        checksum += cur_pos * start_id
-        end[cur_pos] = hex(start_id)[-1]
-        print(f"center: {cur_pos} * {start_id} = {checksum}")
-        cur_pos += 1
-    '''
-    '''
-    for i in range(max_id, len(line), 2):
-        
-        s_file = int(line[i])
-        s_space = int(line[i+1])
+    #for i in range(keys[-1]):
+    #    out += end[i] if i in keys else '.'
 
-        for _ in range(s_space):
-
-            if len(block_queue) == 0:
-                break
-
-            block = block_queue.popleft()
-            checksum += block * cur_pos
-            print(f"end-queue: {cur_pos} * {block} = {checksum}")
-            end[cur_pos] = hex(block)[-1]
-            cur_pos += 1
-
-        if len(block_queue) == 0:
-            break
-    '''
-    for i in sorted(end):
-        print(end[i], end='')
-
+    #print(out)
     print()
     print(f"Checksum = {checksum}")
 
